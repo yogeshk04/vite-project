9f719090bf82046fc0633789570fb4263faf59bc
yogesh nikam <yogesh.nikam@siemens.com>
2024-07-18 11:22:01 +0200

    Food component

---
+++ src/Food.jsx
@@ -0,0 +1,15 @@
+
+function Food() {
+    const food1 = 'Apple';
+    const food2 = 'Banana';
+  
+    return (
+        <ul>
+            <li>Orange</li>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    );
+}
+
+export default Food;
