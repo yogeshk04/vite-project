96c3026298a1836e86e2dc4548e6e9d812d10122
yogesh nikam <yogesh.nikam@siemens.com>
2024-07-18 11:16:08 +0200

    Header and footer component

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
